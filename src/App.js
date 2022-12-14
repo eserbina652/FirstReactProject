174d64faf97e741465f5a5f25309cafadf6abddb
Yevheniia <eserbina652@gmail.com>
2022-12-14 12:34:47 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,60 +2,27 @@ import LeftCard from "./pages/ticketsValidations/left-card/LeftCard";
 import TicketList from "./pages/ticketsValidations/right-card/components/TicketList";
 import "./app.css";
 import React, { useEffect, useState } from "react";
-import res from "./pages/ticketsValidations/right-card/response";
 import Popup from "./pages/popup/Popup";
 import PopupSuccess from "./pages/successfulPopup/PopupSuccess";
+import { useDispatch, useSelector } from "react-redux";
+import { onResetFilter, onSelectFilter } from "./store/reducers/tickets";
 
 function App() {
-  const [ticketList, setTicketList] = useState(res);
-  const [filter, setFilter] = useState([]);
+  const data = useSelector((state) => state.tickets.ticketsData);
+  const filter = useSelector((state) => state.tickets.filters);
+  const dispatch = useDispatch();
+
   const [value, setValue] = useState("UAH");
   const [popupData, setPopupData] = useState(null);
   const [popupSuccess, setPopupSuccess] = useState(false);
 
-  // useEffect(() => {
-  //   if ()
-  // })
-
   useEffect(() => {
     if (filter.length) {
-      onSortByTransfers();
+      dispatch(onSelectFilter());
+    } else {
+      dispatch(onResetFilter());
     }
   }, [filter.length]);
-
-  const onSort = () => {
-    setTicketList((prevState) => {
-      const temp = [...prevState];
-      temp.sort((a, b) => a.stops - b.stops);
-      console.log("temp", temp);
-      return temp;
-    });
-  };
-
-  const onSortByTransfers = () => {
-    setTicketList(res.filter((el) => filter.includes(el.stops)));
-  };
-
-  const onSetFilter = (num) => {
-    // console.log("zdes");
-    if (num || num === 0) {
-      if (filter?.includes(num)) {
-        setFilter((prevState) => prevState?.filter((el) => el !== num));
-      } else {
-        setFilter((prevState) => [...prevState, num]);
-      }
-    }
-  };
-
-  const onClickAll = () => {
-    setTicketList(res);
-  };
-
-  const onChangeValue = (str) => {
-    setValue(str);
-
-    console.log(str);
-  };
 
   const addValueInPopup = (item) => {
     setPopupData(item);
@@ -72,7 +39,7 @@ function App() {
     }
     setTimeout(() => {
       setPopupSuccess(false);
-    }, 10000);
+    }, 3000);
   };
 
   console.log(filter);
@@ -88,17 +55,12 @@ function App() {
       )}
       {popupSuccess && <PopupSuccess closePopup={closePopup} />}
       <div className="all-wrapper">
-        <LeftCard
-          onClickAll={onClickAll}
-          onChangeValue={onChangeValue}
-          onSetFilter={onSetFilter}
-          onSort={onSort}
-        />
+        <LeftCard onChangeValue={onChangeValue} />
         <TicketList
           closePopup={closePopup}
           addValueInPopup={addValueInPopup}
           value={value}
-          ticketList={ticketList}
+          ticketList={data}
         />
       </div>
     </>
@@ -109,15 +71,7 @@ export default App;
 
 /*
 App.js
-import LeftCard from "./pages/ticketsValidations/left-card/LeftCard";
-import TicketList from "./pages/ticketsValidations/right-card/components/TicketList";
-import "./app.css";
-import React, { useEffect, useState } from "react";
-************** удалили рез из импорта *************
-import Popup from "./pages/popup/Popup";
-import PopupSuccess from "./pages/successfulPopup/PopupSuccess";
-************** Импортируем useDispatch, useSelector деструктуризируя from "react-redux" ****************
-************** Таким же способом импортируем onResetFilter, onSelectFilter **************
+
 
 function App() {
   удалили состояние билетов, так как теперь есть общий стейт
@@ -127,17 +81,7 @@ function App() {
   указываем стейт к которому и обращаемся в теле функции
   доставая значение по следующим ссылкам: стейт.билеты.данные билетов
 
-  useSelector - хук из библиотеки redux, который позволяет
-  использовать любое значение со стейта
 
-  удалили состояние фильтров по той же причине
-  вместо состояние фильтров создаем константу(filters)
-  в которой будут храниться данные полученные в результате
-  взаимодействия пользователя со страницей.
-  Вызываем useSelector в котором объявляем стейт и обращаемся
-  к нужному значению по ссылкам стейт.билеты.фильтры
-
-  так же создаем константу dispatch с помощью встроенной в редакс функции useDispatch()
   dispatch - это метод который передает actionы в redux
 
   следующие константы остаются неизмененными, так как
--- src/pages/ticketsValidations/right-card/components/ticketsComponents/footer/TicketsFooter.jsx
+++ src/pages/ticketsValidations/right-card/components/ticketsComponents/footer/TicketsFooter.jsx
@@ -41,3 +41,51 @@ const TicketsFooter = ({
 };
 
 export default TicketsFooter;
+/*
+import React from "react";
+import { companyName } from "../../../../../../assets";
+import "./footer.css";
+import BuyButton from "../../../../../../components/Button/BuyButton";
+import {
+  getCurrencySymbol,
+  getPrise,
+} from "../../../../../../utils/functionFromTicket.js";
+
+Удаляем ончейнжвелью и стр
+const TicketsFooter = ({
+  addValueInPopup,
+  item,
+  value,
+}) => {
+  return (
+    <div>
+      <div className="tickets-footer">
+        <img
+          className="footers-picture"
+          src={companyName}
+          alt="Эта дичь не отображается"
+        />
+
+        Удаляем onChangeValue(str);
+
+        <BuyButton
+          onClick={() => {
+            addValueInPopup(item);
+          }}
+
+          Удаляем onChangeValue={onChangeValue}
+
+          getPrise={getPrise(value, item)}
+          getCurrencySymbol={getCurrencySymbol(item)}
+          addValueInPopup={addValueInPopup}
+          item={item}
+          value={value}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TicketsFooter;
+
+ */
