4d5fc89c85fc5b0da695b65cf39ab0df58b06737
Yevheniia <eserbina652@gmail.com>
2022-12-14 11:15:52 +0100

    changes

--- src/pages/ticketsValidations/right-card/components/Ticket.jsx
+++ src/pages/ticketsValidations/right-card/components/Ticket.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import "../rightCard.css";
-import {
-  getPrise,
-  getCurrencySymbol,
-} from "../../../../utils/functionFromTicket.js";
-// import { getCurrencySymbol } from "../../../../utils/functionFromTicket.js";
-
 import TicketsFooter from "./ticketsComponents/footer/TicketsFooter";
 import TicketsHeader from "./ticketsComponents/header/TicketsHeader";
 
@@ -26,3 +20,29 @@ const Ticket = ({ item, value, addValueInPopup, onChangeValue }) => {
 };
 
 export default Ticket;
+
+/*
+import React from "react";
+import "../rightCard.css";
+
+import TicketsFooter from "./ticketsComponents/footer/TicketsFooter";
+import TicketsHeader from "./ticketsComponents/header/TicketsHeader";
+Удалить onChangeValue
+const Ticket = ({ item, value, addValueInPopup }) => {
+  return (
+    <>
+      <div className="right-ticket">
+        <TicketsHeader item={item} />
+        Удалить onChangeValue
+        <TicketsFooter
+          item={item}
+          value={value}
+          addValueInPopup={addValueInPopup}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Ticket;
+*/
--- src/pages/ticketsValidations/right-card/components/TicketList.jsx
+++ src/pages/ticketsValidations/right-card/components/TicketList.jsx
@@ -20,3 +20,32 @@ const TicketList = ({ ticketList, value, addValueInPopup, onChangeValue }) => {
 };
 
 export default TicketList;
+/*
+
+/*
+TicketList.jsx
+import React from "react";
+import Ticket from "./Ticket";
+
+Удаляем onChangeValue так как ее больше нет
+const TicketList = ({ ticketList, value, addValueInPopup}) => {
+  return (
+    <>
+      <div>
+        {ticketList.map((e, index) => (
+        Удаляем onChangeValue по той же причине
+          <Ticket
+            onChangeValue={onChangeValue}
+            addValueInPopup={addValueInPopup}
+            value={value}
+            item={e}
+            key={index.toString()}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default TicketList;
+ */
