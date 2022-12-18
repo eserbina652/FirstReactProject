6458115c80788255f8be5f2e4cbc16886c1b867a
Yevheniia <eserbina652@gmail.com>
2022-12-18 10:42:18 +0100

    sortButton

--- src/pages/ticketsValidations/left-card/LeftCard.js
+++ src/pages/ticketsValidations/left-card/LeftCard.js
@@ -3,13 +3,24 @@ import ValutaButton from "./components/valutaButtons/ValutaButton";
 import TicketsFilter from "./components/checkbox/TicketsFilter";
 import "./leftcard.css";
 import SortButton from "../../../components/Button/SortButton";
+import { useDispatch, useSelector } from "react-redux";
+import { onSelect } from "../../../store/reducers/tickets";
 
 const LeftCard = () => {
+  const ticketsData = useSelector((state) => state.tickets.ticketsData);
+  const dispatch = useDispatch();
+  const onSort = (data) => {
+    dispatch(onSelect(data));
+  };
   return (
     <div className="left-card-wrapper">
       <ValutaButton />
       <TicketsFilter />
-      <SortButton title={"Sort"} styles={"_sortButton"} />
+      <SortButton
+        onClick={() => onSort(ticketsData)}
+        title={"Sort"}
+        styles={"_sortButton"}
+      />
     </div>
   );
 };
