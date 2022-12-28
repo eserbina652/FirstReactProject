ee82c75b6d7df429eaa155bff39d99f7ecc4c04f
Yevheniia <eserbina652@gmail.com>
2022-12-28 18:01:25 +0100

    ReduxRouter start

--- src/App.js
+++ src/App.js
@@ -26,3 +26,11 @@ function App() {
 }
 
 export default App;
+
+/*
+BrowserRouter - it is wrapper
+The element that is at the top is always visible on each page
+We must wrap pages in Rotes tag, don't forget to specify the way of page
+in attribute 'path' and attribute to in first el which always visible and
+to specify (attribute with name 'element') the el to which this way belongs.
+ */
