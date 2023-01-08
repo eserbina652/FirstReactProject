864638663f5d5a220994e8946637d80b19bcb4af
yevheniia <eserbina652@gmail.com>
2023-01-08 14:26:27 +0100

    installing libraries router

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import React from "react";
 import Popup from "./pages/popup/Popup";
 import PopupSuccess from "./pages/successfulPopup/PopupSuccess";
 import { useSelector } from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   const data = useSelector((state) => state.tickets.ticketsData);
   const isOpenSuccess = useSelector((state) => state.popups.successVisible);
   const popupItem = useSelector((state) => state.popups.item);
   return (
-    <>
+    <BrowserRouter>
+
       {popupItem && <Popup />}
       {isOpenSuccess && <PopupSuccess />}
       <div className="all-wrapper">
         <LeftCard />
         <TicketList ticketList={data} />
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
