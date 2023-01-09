b6f41361676718feaff6d99a53abd5d6e461d098
yevheniia <eserbina652@gmail.com>
2023-01-09 14:55:53 +0100

    CryptoCard.jsx

--- src/screens/Crypto/cryptoCards/CryptoCard.jsx
+++ src/screens/Crypto/cryptoCards/CryptoCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './cryptoCards/cryptoCard.css'
+import './cryptoCard.css'
 
 const CryptoCard = ({item}) => {
     return (
         <div className="cryptoElements-wrapper">
+            <img className="cryptoIcon" src={item.image} alt={`${item.name} picture`}/>
             <h3 className="cryptoName">{item.name}</h3>
+            <p>Current price: {item.current_price} {item.symbol}</p>
         </div>
     );
 };
